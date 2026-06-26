9f97ec61f665fc6e703f86e5669220f5a80ceb7c
kn <wjdg159@gmail.com>
2026-06-27 05:21:59 +0900

    leetcode day4 완료

--- leetcode/day-04-concatenation-of-array/solution.py
+++ leetcode/day-04-concatenation-of-array/solution.py
@@ -1,3 +1,9 @@
 class Solution:
     def getConcatenation(self, nums: list[int]) -> list[int]:
-        pass
+        ans: list[int] = [0] * (len(nums) * 2)
+        for i, num in enumerate(nums):
+            ans[i] = num
+            ans[i + len(nums)] = num
+
+        return ans    
+        # return nums + nums    가장좋은 답변
